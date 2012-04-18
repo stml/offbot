88650cfaa32c7a481da766d0dec7ba0b49fefbc6
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-18 16:23:14 +0100

    it would help if I called things they way they were named

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	attr_accessible :name, :people, :person_ids
   has_and_belongs_to_many :people
-  has_many :updates, :emails
+  has_many :updates, :email_messages
   has_and_belongs_to_many :invitations
   validates_presence_of :name
 end
