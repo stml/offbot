933a5d3fc84d85f2b7ede162cd235a6b53179959
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-18 11:05:35 +0100

    route change to simplify sognup

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Offbot::Application.routes.draw do
   resources :people
   resources :updates
 
-  devise_for :people, :path => "you", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'sign_up' }
+  devise_for :people, :path => "you", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :sign_up => 'sign_up' }
 
 
   # Sample resource route with options:
