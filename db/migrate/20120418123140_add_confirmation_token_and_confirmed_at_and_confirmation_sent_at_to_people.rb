cca7d07bbcb4bd381e5d96b78bad6682078bda65
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-18 13:33:02 +0100

    added missing migration for confirming email

---
+++ db/migrate/20120418123140_add_confirmation_token_and_confirmed_at_and_confirmation_sent_at_to_people.rb
@@ -0,0 +1,17 @@
+class AddConfirmationTokenAndConfirmedAtAndConfirmationSentAtToPeople < ActiveRecord::Migration
+  def self.up
+    change_table(:people) do |t|
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+    end
+
+  end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418114438) do
+ActiveRecord::Schema.define(:version => 20120418123140) do
 
   create_table "invitations", :force => true do |t|
     t.string   "email"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(:version => 20120418114438) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
   add_index "people", ["email"], :name => "index_people_on_email", :unique => true
