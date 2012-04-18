a2e7d6c93773d5a5252248ce7e30cefa7468cd19
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-18 15:17:37 +0100

    set up action mailer to use smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Offbot::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
