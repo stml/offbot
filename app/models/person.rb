bc75d2184c2b76ff5382dfdb28398f711b315bc5
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-18 13:28:29 +0100

    cleaning

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,11 +2,10 @@ class Person < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable, :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable,
+         :recoverable, :rememberable, :trackable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :email, :name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   has_many :updates
   has_and_belongs_to_many :projects
   validates_presence_of :email, :name
