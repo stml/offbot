5da019c94556b7e1d5fbf0a1ecdb7f5eb172d48d
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-19 12:18:58 +0100

    typo

--- app/mailers/request_update.rb
+++ app/mailers/request_update.rb
@@ -1,6 +1,6 @@
 class RequestUpdate < ActionMailer::Base 
   def request_update(email_message)
     @person = email_message.person
-    mail(:to => @person.email, :from => "offbott-#{@email_message.message_id}@offbott.com", :subject => "Update request")
+    mail(:to => @person.email, :from => "offbott-#{email_message.message_id}@offbott.com", :subject => "Update request")
   end
 end
