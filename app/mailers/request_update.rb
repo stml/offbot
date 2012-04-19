f2e3d947298287cc8a7d3c43a2ba3170d36e1b6a
Natalia Buckley <natalia.buckley@gmail.com>
2012-04-19 12:21:55 +0100

    always helps when I name variables correctly

--- app/mailers/request_update.rb
+++ app/mailers/request_update.rb
@@ -1,6 +1,7 @@
 class RequestUpdate < ActionMailer::Base 
   def request_update(email_message)
     @person = email_message.person
+    @name = email_message.person.name
     mail(:to => @person.email, :from => "offbott-#{email_message.message_id}@offbott.com", :subject => "Update request")
   end
 end
