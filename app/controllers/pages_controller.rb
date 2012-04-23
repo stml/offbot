68accf7d1a0d89d8b5c2893c92a0eadc50111674
James Bridle <james@shorttermmemoryloss.com>
2012-04-23 20:03:03 +0100

    Added an about page but can't quite figure out how to include layouts

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ActionController::Base
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Offbot::Application.routes.draw do
   devise_for :people, :path => "you", :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'sign_up' }
 
   match "/sendmail/incoming" => "listener#receive_email", :via => :post
+  match '/about' => 'pages#about'
 
   # Sample resource route with options:
   #   resources :products do
