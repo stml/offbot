cddd97b324c32356fe22e8bab0f8c987c8ab2ace
James Bridle <james@shorttermmemoryloss.com>
2012-04-24 12:21:15 +0100

    About page and registration path fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
-class PagesController < ActionController::Base
+class PagesController < ApplicationController
+  skip_before_filter :authenticate_person!
   def about
+      respond_to do |format|
+      format.html
+    end
   end
 end
