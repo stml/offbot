f45dec6614ef6bb90de14fcc0972248977dd8230
Natalia Buckley <natalia.buckley@gmail.com>
2012-05-01 11:18:42 +0100

    make sure to send out invites doh

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,18 @@
 class Invitation < ActiveRecord::Base
-  attr_accessible :email
-  has_and_belongs_to_many :projects
-  validates_presence_of :email
+	attr_accessible :email
+	has_and_belongs_to_many :projects
+	validates_presence_of :email
+
+	after_create :add_to_projects
+
+	private
+
+	def add_to_projects
+		invitation = Invitation.find_by_email(self.email)
+			if invitation	
+				invitation.projects.each do |project|
+				self.projects << project	
+			end
+		end	
+	end
 end
