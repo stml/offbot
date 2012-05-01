d8c5e895d5fb7587b598766f1ec799945996c2c8
ntlk <natalia.buckley@gmail.com>
2012-05-01 12:13:54 +0100

    created a mailer for invites

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,10 @@ class Invitation < ActiveRecord::Base
 	attr_accessible :email
 	has_and_belongs_to_many :projects
 	validates_presence_of :email
+
+	after_create :send_out_invite
+
+	def send_out_invite
+		SendInvitation.send_out_invite(self).deliver
+	end
 end
