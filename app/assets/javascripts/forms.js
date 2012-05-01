eacd94395fe2f982ac8d5ec746eae5508459504c
James Bridle <james@shorttermmemoryloss.com>
2012-05-01 17:02:10 +0100

    Nice input fields with jquery onfocus

---
+++ app/assets/javascripts/forms.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$(".clear_field").focus(function() {
+      	$(this).val('');
+      	$(this).css('color','black');
+      	$(this).css('font-style','normal');
+      	});
+	});
