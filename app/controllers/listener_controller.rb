3cf40e3cf6e26eb1d30a0c588b6cda02a3faa213
ntlk <natalia.buckley@gmail.com>
2012-05-14 10:08:42 +0100

    debugging

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -10,7 +10,7 @@ class ListenerController < ApplicationController
 		message_id = sent_to[0].split('.')[1]
 		sent_by = params["from"].split('<')[1].chop
 
-		puts "Message id: #{message_id}"
+		puts "Message id: #{message_id}, project: #{sent_to[0].split('.')[0]}"
 		puts "Sent by: #{sent_by}"
 
 		# if Person.find_by_email_key(message_id)
