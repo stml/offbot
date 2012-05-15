03fc9624cc31f1641e8a8af3d9230079512c5572
ntlk <natalia.buckley@gmail.com>
2012-05-15 15:58:39 +0100

    fixing encoding

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -1,4 +1,5 @@
 class ListenerController < ApplicationController
+	require 'iconv'
 	skip_before_filter :verify_authenticity_token
 	skip_before_filter :authenticate_person!
 	
@@ -17,7 +18,7 @@ class ListenerController < ApplicationController
 		puts params["headers"]
 
 		unless (params["text"].encoding == "UTF-8") or (params["text"].encoding == "utf-8")
-			text = params["text"].convert_to_utf8
+			text = params["text"]..encode("utf-8", :invalid => :replace, :undef => :replace)
 		else
 			text = params["text"]
 		end
