63f03e041c6fac937bf55165319fa9b5e0a3b317
James Bridle <james@shorttermmemoryloss.com>
2012-05-16 16:14:54 +0100

    invitation emails with project creators

--- app/mailers/send_invitation.rb
+++ app/mailers/send_invitation.rb
@@ -2,12 +2,14 @@ class SendInvitation < ActionMailer::Base
   default :from => "joinoffbott@#{DOMAIN}", :css => :email
   def send_out_invite(invite)
     @email = invite.email
+    @projects = invite.projects
     mail(:to => @email, :from => "joinoffbott@#{DOMAIN}", :subject => "Hi! Someone invited you to join Offbott")
   end
 
   class Preview < MailView
     def send_out_invite
     	@invite = Invitation.last
+    	@projects = @invite.projects
     	::SendInvitation.send_out_invite(@invite)
     end
 
