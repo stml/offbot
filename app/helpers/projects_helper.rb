95d04188a0ab069bc758a7254b8c2ff0c5612107
ntlk <natalia.buckley@gmail.com>
2012-05-16 09:40:41 +0100

    counters show the number of updates on a specific project. fixes #29

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,12 @@
 module ProjectsHelper
+	def updates_count(person, project)
+		# count number of updates per project
+		count = 0
+		for update in project.updates
+			if update.person == person
+				count = count + 1
+			end
+		end
+		count
+	end
 end
