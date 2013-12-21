4c81da291bb906d1c8902d3aa3246e73b0579abb
ntlk <natalia.buckley@gmail.com>
2013-12-21 18:19:29 +0000

    reorganise all the things

--- spec/scheduled_requests_methods_spec.rb
+++ spec/scheduled_requests_methods_spec.rb
@@ -17,9 +17,12 @@ describe ScheduledRequestsMethods do
       Project.create! \
         name: 'example',
         created_by: person.id,
-        frequency: 0,
         archived: false
     }
+
+    let(:start_time) { Time.local(2013, 12, 22).beginning_of_day }
+    let(:end_time) { start_time + duration }
+    let(:emails) { scheduled_emails_sent_between(start_time, end_time).select { |email| email.subject == "[#{project.name}] Hello, it's Offbott again" and email.to.include?(person.email)} }
 
     specify 'there should be one person' do
       Person.all.should have(1).person
@@ -37,11 +40,11 @@ describe ScheduledRequestsMethods do
       person.projects.should include(project)
     end
 
-    context 'after a week has elapsed' do
-      let(:start_time) { Time.local(2013, 12, 22).beginning_of_day }
+    context 'when the project has a daily frequency' do
       let(:duration) { 1.week }
-      let(:end_time) { start_time + duration }
-      let(:emails) { scheduled_emails_sent_between(start_time, end_time).select { |email| email.subject == "[#{project.name}] Hello, it's Offbott again" and email.to.include?(person.email)} }
+      before(:each) do
+        project.update_attribute(:frequency, 0)
+      end
 
       specify 'six emails have been sent' do # because it currently delivers on a Saturday too
         emails.should have(6).messages
@@ -52,4 +55,7 @@ describe ScheduledRequestsMethods do
       end
     end
   end
+
+
 end
+
