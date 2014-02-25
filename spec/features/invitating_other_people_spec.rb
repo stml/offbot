fe8e330bd3d0b00935df0e970539c8cdd52ed38f
ntlk <natalia.buckley@gmail.com>
2014-02-25 22:13:09 +0000

    Move things around

--- spec/features/invitating_other_people_spec.rb
+++ spec/features/invitating_other_people_spec.rb
@@ -6,27 +6,19 @@ feature 'inviting other people' do
     @owner = create :person
     @project = create :project, created_by: @owner.id
     @email_address = 'invitee@example.com'
-  end
-
-  scenario 'person invites someone to a project and an invite email is sent'  do
     login_as(@owner, scope: :person)
     visit project_path(@project)
     click_on ('Edit this project')
     all('#emails_')[0].set(@email_address)
     click_on('Save this project')
+  end
 
+  scenario 'person invites someone to a project and an invite email is sent'  do
     open_email(@email_address)
     expect(current_email.subject).to eq 'Hi! Someone invited you to join Offbott'
   end
 
   scenario 'person invites someone to a project and they join'  do
-    login_as(@owner, scope: :person)
-    visit project_path(@project)
-    click_on ('Edit this project')
-    all('#emails_')[0].set(@email_address)
-    click_on('Save this project')
-    logout(:person)
-
     open_email(@email_address)
     current_email.click_on('Offbott.com')
     ActionMailer::Base.deliveries.clear
