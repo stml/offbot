3f62076b41369c8de031b67383aac29452cae28f
ntlk <natalia.buckley@gmail.com>
2014-02-25 22:12:20 +0000

    Test that a person invited can sign up

--- spec/features/invitating_other_people_spec.rb
+++ spec/features/invitating_other_people_spec.rb
@@ -19,4 +19,30 @@ feature 'inviting other people' do
     expect(current_email.subject).to eq 'Hi! Someone invited you to join Offbott'
   end
 
+  scenario 'person invites someone to a project and they join'  do
+    login_as(@owner, scope: :person)
+    visit project_path(@project)
+    click_on ('Edit this project')
+    all('#emails_')[0].set(@email_address)
+    click_on('Save this project')
+    logout(:person)
+
+    open_email(@email_address)
+    current_email.click_on('Offbott.com')
+    ActionMailer::Base.deliveries.clear
+    click_on('register a new account')
+    fill_in('Name', with: 'Max Invitee')
+    fill_in('Email', with: @email_address)
+    fill_in('Password', with: 'password')
+    fill_in('Password confirmation', with: 'password')
+    click_on('Sign up')
+
+    open_email(@email_address)
+    current_email.click_on('Confirm my Offbott account')
+
+    invitee = Person.find_by_email(@email_address)
+    expect(@project.people).to include(invitee)
+
+  end
+
 end
