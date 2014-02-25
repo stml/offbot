379bd34cb9976ef4a4977f8e899f0ad62e906beb
ntlk <natalia.buckley@gmail.com>
2014-02-25 21:56:37 +0000

    Pull email_address into the before block so it can be reused

--- spec/features/invitating_other_people_spec.rb
+++ spec/features/invitating_other_people_spec.rb
@@ -5,17 +5,17 @@ feature 'inviting other people' do
   before do
     @owner = create :person
     @project = create :project, created_by: @owner.id
+    @email_address = 'invitee@example.com'
   end
 
   scenario 'person invites someone to a project and an invite email is sent'  do
-    email = 'invitee@example.com'
     login_as(@owner, scope: :person)
     visit project_path(@project)
     click_on ('Edit this project')
-    all('#emails_')[0].set(email)
+    all('#emails_')[0].set(@email_address)
     click_on('Save this project')
 
-    open_email(email)
+    open_email(@email_address)
     expect(current_email.subject).to eq 'Hi! Someone invited you to join Offbott'
   end
 
