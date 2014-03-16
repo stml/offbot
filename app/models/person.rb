f8a7767719b61f042d55e22a5a745034707ec547
ntlk <natalia.buckley@gmail.com>
2014-03-16 20:14:58 +0000

    Kill comments

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,7 @@
 class Person < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable, :validatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :project_ids, :email_key
   has_many :updates
   has_many :email_messages
