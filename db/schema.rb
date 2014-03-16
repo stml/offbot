33b2dc113c6db7d40a49df11234f3302b97c6b96
ntlk <natalia.buckley@gmail.com>
2014-03-16 20:33:04 +0000

    Add token to invitations and test that it gets generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140316194532) do
+ActiveRecord::Schema.define(:version => 20140316202100) do
 
   create_table "email_messages", :force => true do |t|
     t.string   "message_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20140316194532) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "token"
   end
 
   create_table "invitations_projects", :id => false, :force => true do |t|
