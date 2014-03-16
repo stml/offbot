3952f0d855aecf31d37fa40919dbc2722b927211
ntlk <natalia.buckley@gmail.com>
2014-03-16 20:11:19 +0000

    Spread before filters on separate lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_person!, :mailer_set_url_options
+  before_filter :authenticate_person!
+  before_filter :mailer_set_url_options
 
   helper :all
 
